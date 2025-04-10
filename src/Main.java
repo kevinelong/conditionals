d1db59e9c601d0a2a686c2d68d9adfc984a497cd
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-10 09:34:56 -0700

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void go(int age){
+        if(age >= 18){
+            System.out.println("GO VOTE!!!");
+        }else{
+            System.out.println("GO PLAY");
         }
+    }
+
+    public static void respond(char answer){
+        if(answer == 'Y' || answer == 'y'){
+            System.out.println("YUP YUP YUP");
+        }else{
+            System.out.println("NOPE NOPE NOPE");
+        }
+    }
+
+    public static void drink(int age) {
+        if(age < 21){
+            System.out.println("juice");
+        }else if(age < 50) {
+            System.out.println("adult beverage");
+        }else if(age < 77) {
+            System.out.println("tea");
+        }else{
+            System.out.println("heavy beverage");
+        }
+    }
+
+    public static void main(String[] args) {
+        drink(11); // juice
+        drink(25); // adult beverage
+        drink(51); // tea
+        drink(99); // heavy beverage
+
+        for(String a : args) {
+            System.out.println(a);
+        }
+
+        go(13);
+        go(21);
+
+        respond('Y');
+        respond('N');
+
     }
 }
